43aa943970ed35cea1de362d80f1d045f58f095f
DarioLoForti <lofortidario@gmail.com>
2024-03-07 10:55:18 +0100

    includo paginazione

--- src/pages/AppCars.vue
+++ src/pages/AppCars.vue
@@ -20,9 +20,15 @@ export default {
         this.getCars();
     },
     methods: {
-        getCars(){
-            axios.get(`${this.store.baseUrl}/api/cars`).then((response)=>{
-                this.cars = response.data.response;
+        getCars(page_number){
+            axios.get(`${this.store.baseUrl}/api/cars`,{
+                params: {
+                    page: page_number
+                }
+            }).then((response)=>{
+                this.cars = response.data.response.data;
+                this.currentPage = response.data.response.current_page
+                this.lastPage = response.data.response.last_page
             })
         },
     },
@@ -38,6 +44,15 @@ export default {
             </div>
             <div class="row">
                 <CarCard v-for="car, index in cars" :key="index" :car="car"/>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button :class="currentPage == 1 ? 'disabled' : '' " class="btn btn-sm btn-square" @click="getCars(currentPage - 1 ) ">
+                     precedente
+                </button>
+                <button :class="currentPage == lastPage ? 'disabled' : '' " class="btn btn-sm btn-square" @click="getCars(currentPage + 1 ) ">
+                    successivo
+                </button>
+               
             </div>
         </div>
     </div>
